e1ea868c3828f5d931e108aa157ec5c6f74b1a85
mccannsc <mccannsc@gmail.com>
2012-05-22 19:16:22 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VolteeApp::Application.config.secret_token = '013b24858fcab58121bc0b917e7d08ff247b6824fabcfc60ca85461ea9334de986445df0482e67f15c6ddb6871240a3c43bc2aeb96a28637759752824c6dac19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+VolteeApp::Application.config.session_store :cookie_store, key: '_voltee_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# VolteeApp::Application.config.session_store :active_record_store
